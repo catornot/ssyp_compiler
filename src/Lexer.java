8bf39ec727271e4979c5ce580a75b76a95176a93
cat_or_not <41955154+catornot@users.noreply.github.com>
2024-07-15 15:03:05 +0700

    Translator updates and new lexer (#1)
    * 2+2=5 (#3)
    
    * Add files via upload
    
    * Starting the parser.
    
    * Добавил Parser
    
    * fix mistakes
    
    * Начал тест парсера.
    
    * Завершил тест и починил парсер.
    
    * Небольшие исправления.
    
    ---------
    
    Co-authored-by: cat_or_not <41955154+catornot@users.noreply.github.com>
    
    * Ready Lexer, which will be used. (#4) (#5)
    
    * Add files via upload
    
    * Повторное отправление на ревью Lexer, и исправление замены elsOfSplitByLinebreakAndSpase.toCharArray() на toCharElsOfSplitEnd.
    
    * Normal Lexer.
    
    * Lexer to Compiler.
    
    * fix warnings
    
    * fix warnings
    
    * fix warnings
    
    * fix warnings
    
    * Ready Lexer.
    
    * Ready Lexer.
    
    ---------
    
    Co-authored-by: pogsandro25 <abaskagenrih@gmail.com>
    Co-authored-by: pogsandro25 <pogsandro@gmail.com>
    
    * Introduce a new lexer and remove merge artifacts
    
    * bytecode generation + improved Translator
    
    ---------
    
    Co-authored-by: Vsevolod <100917853+VDVVAH@users.noreply.github.com>
    Co-authored-by: pogsandro25 <abaskagenrih@gmail.com>
    Co-authored-by: pogsandro25 <pogsandro@gmail.com>

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,62 +1,53 @@
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
-import java.util.stream.Stream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Lexer {
-    private String code = null;
-    private final String[] key_combs;
-    public String[] separators = {" ", "\n", "  "};
-    public Boolean DO_STRIP = true;
-    public Lexer(String[] key_combs) {
-        this.key_combs = key_combs;
-    }
+    public static List<String> tokenize(String input) {
+        List<String> tokens = new ArrayList<>();
+        String splitBySemicolon = input.replace(";", ";\n");
+        String[] splitByLine = splitBySemicolon.split("\n");
+        List<String> splitSpcLine = new ArrayList<>();
+        for (String elementOfSplitByLine : splitByLine) {
+            String[] splitBySpc = elementOfSplitByLine.split(" ");
+            splitSpcLine.addAll(Arrays.asList(splitBySpc));
+        }
+        for (String elementOfSplitSpcLine : splitSpcLine) {
+            boolean isStartsWithSpecialSymbol = (elementOfSplitSpcLine.charAt(0) == '#' || elementOfSplitSpcLine.charAt(0) == '@');
+            if (isStartsWithSpecialSymbol && elementOfSplitSpcLine.charAt(elementOfSplitSpcLine.length() - 1) == ';') {
+                tokens.add(String.valueOf(elementOfSplitSpcLine.charAt(0)));
+                tokens.add(elementOfSplitSpcLine.substring(1, elementOfSplitSpcLine.length() - 1));
+                tokens.add(elementOfSplitSpcLine.substring(elementOfSplitSpcLine.length() - 1));
+            } else if (isStartsWithSpecialSymbol) {
+                tokens.add(String.valueOf(elementOfSplitSpcLine.charAt(0)));
+                tokens.add(elementOfSplitSpcLine.substring(1));
+            } else if (elementOfSplitSpcLine.charAt(elementOfSplitSpcLine.length() - 1) == ';') {
+                tokens.add(elementOfSplitSpcLine.substring(0, elementOfSplitSpcLine.length() - 1));
+                tokens.add(elementOfSplitSpcLine.substring(elementOfSplitSpcLine.length() - 1));
 
-
-    public Stream<String> tokenize() {
-        ArrayList<String> lexemes = new ArrayList<>(), code_fragments = new ArrayList<>();
-        lexemes.add(this.code);
-        for (String sep: this.separators) {
-            for (String elm: lexemes) {
-                if (elm != null) {
-                    Collections.addAll(code_fragments, elm.split(sep));
-                }
+            } else {
+                tokens.add(elementOfSplitSpcLine);
             }
         }
-        lexemes = code_fragments;
-        for (String comb: this.key_combs) {
-            for (int k = 0; k < lexemes.size(); k++) {
-                String lexeme = lexemes.get(k);
-                if (lexeme.contains(comb) && !lexeme.equals(comb)) {
-                    int index = lexemes.get(k).indexOf(comb);
-                    lexemes.remove(k);
-                    lexemes.add(k, lexeme.substring(index + 1));
-                    lexemes.add(k, comb);
-                    lexemes.add(k, lexeme.substring(0, index));
-                }
-            }
-        }
-        if (this.DO_STRIP) {
-            lexemes.replaceAll(String::strip);
-        }
-        return lexemes.stream().filter(elm-> !Objects.equals(elm, ""));
+        return tokens;
     }
 
-    public List<String> getTokens(String filename) {
-        String text = null;
-        ArrayList<String> lines = new ArrayList<>();
+    // TODO refactor this
+    public static List<String> tokenizeFromFile(String filename) {
         try {
-            Scanner scanner = new Scanner(new File(filename));
-            while (scanner.hasNextLine()) {
-                lines.add(scanner.nextLine().trim());
-            }
+            return tokenize(Files.readString(Path.of(filename), Charset.defaultCharset()));
         } catch (IOException e) {
             e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        Lexer lexer = new Lexer(
-                this.key_combs
-                );
-        lexer.code = String.join("\n", lines);
-        return lexer.tokenize().toList();
     }
 }
+
+
